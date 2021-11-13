d34f224a8f00b5f5f21e67d4da1d4b4f565a69e4
dazesace <dazemcblaze@gmail.com>
2021-11-13 17:59:32 +0100

    setup colors

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    darkMode: false,
+    theme: {
+        extend: {
+            colors: {
+                'space': '#051923',
+                'sea': '#003554',
+                'gold': '#FEAC48',
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
